23aa6473e8b21da23737dc02d4afddc82ec0b90f
monkeymays <monkeymays@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-08-20 21:33:36 +0000

    git-svn-id: https://plugins.svn.wordpress.org/santas-christmas-countdown/trunk@588010 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,70 @@
+=== Christmas Countdown Widget ===
+
+Contributors: monkeymays
+Donate Link: http://christmaswebmaster.com/santa-claus-christmas-countdown-wordpress-plugin
+Tags: christmas countdown, santa claus, holiday
+Requires at least: 3.0
+Tested up to: 3.4.1
+Stable tag: 1.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Displays a cute Santa Claus Christmas Countdown in your sidebar.
+
+
+
+== Description ==
+
+The Christmas Countdown Widget displays a cute Santa Claus countdown to Christmas in your sidebar. 
+The countdown automatically updates each year and starts the countdown over again on Christmas.
+The Christmas countdown will appear in your sidebar on a white background.
+
+
+
+== Installation ==
+
+
+
+1. Upload `xmascount.php` to the `/wp-content/plugins/` directory
+2. Activate the plugin through the 'Plugins' menu in WordPress
+3. Go to your widgets screen, and drag the Christmas Countdown widget to your sidebar.
+4. Let the countdown to Christmas begin!
+
+
+
+== Frequently Asked Questions ==
+
+
+
+= Can I change the background color? =
+
+
+
+At this time no. We hope to have it appear on a transparent background in the near future.
+
+
+
+
+
+== Screenshots ==
+
+
+
+1. Upload plugin and install.
+
+2. On the plugins panel activate the Christmas Countdown Widget.
+
+3. In the widgets menu drag the Christmas Countdown Widget to your sidebar.
+
+4. The Christmas Countdown Widget displays a cute Santa Claus countdown to Christmas in your sidebar!
+
+
+
+== Changelog ==
+
+Version 1.0 Released 8/20/2012
+
+
+== Upgrade Notice ==
+
+Version 1.0 Released 8/20/2012
---
+++ xmascount.php
@@ -0,0 +1,68 @@
+<?php
+
+
+/*
+Plugin Name: Christmas Countdown Widget
+Plugin URI: http://christmaswebmaster.com/santa-claus-christmas-countdown-wordpress-plugin
+Description: Displays a cute Christmas countdown in your sidebar.
+Author: Monica Mays
+Version: 1.0
+Author URI: http://christmaswebmaster.com/
+
+Christmas Countdown Widget is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or 
+any later version.
+
+Christmas Countdown Widget is distributed in the hope that it will be useful, 
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Christmas Countdown Widget. If not, see <http://www.gnu.org/licenses/>.
+*/
+ 
+ 
+class xmascount extends WP_Widget
+{
+  function xmascount()
+  {
+    $widget_ops = array('classname' => 'xmascount', 'description' => 'Displays a cute Christmas countdown' );
+    $this->WP_Widget('xmascount', 'Christmas Countdown', $widget_ops);
+  }
+ 
+  function form($instance)
+  {
+    $instance = wp_parse_args( (array) $instance, array( 'title' => '' ) );
+    $title = $instance['title'];
+?>
+  <p><label for="<?php echo $this->get_field_id('title'); ?>">Title: <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo attribute_escape($title); ?>" /></label></p>
+<?php
+  }
+ 
+  function update($new_instance, $old_instance)
+  {
+    $instance = $old_instance;
+    $instance['title'] = $new_instance['title'];
+    return $instance;
+  }
+ 
+  function widget($args, $instance)
+  {
+    extract($args, EXTR_SKIP);
+ 
+    echo $before_widget;
+    $title = empty($instance['title']) ? ' ' : apply_filters('widget_title', $instance['title']);
+ 
+    if (!empty($title))
+      echo $before_title . $title . $after_title;;
+ 
+    // WIDGET CODE GOES HERE
+    echo "<iframe frameborder=0 marginheight=0 marginwidth=0 scrolling=no height=191 width=100% src=http://christmaswebmaster.com/plugin-files/SantasCountdown/santas-count.html></iframe>";
+ 
+    echo $after_widget;
+  }
+ 
+}
+add_action( 'widgets_init', create_function('', 'return register_widget("xmascount");') );?>
