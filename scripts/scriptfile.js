a4fc86675565955f02a24fc9c0b6b41fa331060a
monkeymays <monkeymays@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-05-14 17:34:21 +0000

    uncommitted files error if this keeps up I'll need committed
    git-svn-id: https://plugins.svn.wordpress.org/santas-christmas-countdown/trunk@713036 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ scripts/scriptfile.js
@@ -0,0 +1,25 @@
+function cw_axmascount() {today = new Date();
+thismon = today.getMonth();
+thisday = today.getDate();
+thisyr = today.getFullYear();
+if (thismon == 11 && thisday > 25)
+	{
+	thisyr = +thisyr;
+	BigDay = new Date("December 25, "+thisyr);
+	}
+else
+	{
+	BigDay = new Date("December 25, "+thisyr);
+	}
+
+msPerDay = 24 * 60 * 60 * 1000;
+timeLeft = (BigDay.getTime() - today.getTime() - 1);
+e_daysLeft = timeLeft / msPerDay;
+daysLeft = Math.floor(e_daysLeft);
+if (daysLeft <= 0 )
+{ 
+document.write("<br><br>Merry<br>Christmas!")
+}
+else 
+{ 
+document.write( ""+daysLeft+" days <BR> til Christmas!");}}
