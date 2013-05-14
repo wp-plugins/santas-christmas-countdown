514617390c614b4cd9507be5718b172e6dc423fd
monkeymays <monkeymays@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-05-14 17:01:14 +0000

    Updated to version 2
    git-svn-id: https://plugins.svn.wordpress.org/santas-christmas-countdown/trunk@713025 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- readme.txt
+++ readme.txt
@@ -3,27 +3,32 @@
 
 Contributors: monkeymays
 Donate Link: http://christmaswebmaster.com/santa-claus-christmas-countdown-wordpress-plugin
-Tags: christmas countdown, santa claus, holiday, christmas, widget-only, widget, sidebar, countdown
+Tags: christmas countdown, santa claus, holiday, christmas, widget-only, widget, sidebar, countdown, shortcode
 Requires at least: 3.0
 Tested up to: 3.5.1
-Stable tag: 1.3
+Stable tag: 2.0
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
-Displays a cute Santa Claus Christmas Countdown in your sidebar.
+Displays a cute Santa Claus Christmas Countdown in your sidebar. NEW IN VERSION 2.0: Use the shortcode `[countdown]` to display the countdown on any post or page. 
 
 == Description ==
 
-The Christmas Countdown Widget displays a cute Santa Claus counting down to Christmas in your sidebar. On Christmas day Santa displays a "Merry Christmas" greeting to your visitors. Since the countdown automatically updates each year and starts the countdown over again on the day after Christmas, you can leave it in your sidebar all year round if you want! BTW - The Christmas countdown will appear in your sidebar on a transparent background so it looks good with any theme. 
+The Christmas Countdown Widget displays a cute Santa Claus counting down to Christmas in your sidebar. On Christmas day Santa displays a "Merry Christmas" greeting to your visitors. Since the countdown automatically updates each year and starts the countdown over again on the day after Christmas, you can leave it in your sidebar all year round if you want!
+
+By the way... The Christmas countdown will appear in your sidebar on a transparent background so it looks good with any theme.
+
+NEW IN VERSION 2.0: Use the shortcode `[countdown]` to add the countdown to any post or page. 
 
 For more information about Santa's countdown, visit [ChristmasWebmaster.com](http://christmaswebmaster.com/).
 
 == Installation ==
 
-1. Upload `xmascount.php` to the `/wp-content/plugins/` directory
+1. Upload the 'santas-christmas-countdown' folder to your `/wp-content/plugins/` directory, or install via the plugins admin panel.
 2. Activate the plugin through the 'Plugins' menu in WordPress
 3. Go to your widgets screen, and drag the Christmas Countdown widget to your sidebar.
 4. Let the countdown to Christmas begin!
+5. NEW in version 2.0: Use the shortcode `[countdown]` to display the countdown in any post or page.
 
 == Frequently Asked Questions ==
 
@@ -31,13 +36,23 @@ For more information about Santa's countdown, visit [ChristmasWebmaster.com](htt
 
 It is transparent, but you are free to edit the plugin to your needs.
 
+= How do I display the countdown on posts and pages?  =
+
+Use the shortcode `[countdown]` to display the countdown to the left of your text, or use `[countdown-right]` to align the countdown to the right of your text.
+
 == Screenshots ==
 1. Upload plugin and install.
 2. On the plugins panel activate the Christmas Countdown Widget.
 3. In the widgets menu drag the Christmas Countdown Widget to your sidebar.
 4. The Christmas Countdown Widget displays a cute Santa Claus countdown to Christmas in your sidebar!
+5. New in version 2.0 - use the shortcode `[countdown]` to display the countdown on any page or post.
 
 == Changelog ==
+= Version 2.0 Released 05/13/2013  =
+
+ * Restructured plugin files and updated code. Added shortcode so that the countdown can be used on any post or page.
+
+
 = Version 1.3 Released 03/04/2013  =
  
  * Minor update to countdown script.
@@ -59,6 +74,7 @@ It is transparent, but you are free to edit the plugin to your needs.
 
 
 == Upgrade Notice ==
+Version 2.0 Released 05/13/2013
 Version 1.3 Released 03/04/2013
 Version 1.2 Released 11/07/2012
 Version 1.1 Released 10/03/2012
--- xmascount.php
+++ xmascount.php
@@ -5,7 +5,7 @@ Plugin Name: Christmas Countdown Widget
 Plugin URI: http://christmaswebmaster.com/santa-claus-christmas-countdown-wordpress-plugin
 Description: Displays a cute Christmas countdown in your sidebar.
 Author: Monica Mays
-Version: 1.3
+Version: 2.0
 Author URI: http://christmaswebmaster.com/
 
 Christmas Countdown Widget is free software: you can redistribute it and/or modify
@@ -21,14 +21,46 @@ GNU General Public License for more details.
 You should have received a copy of the GNU General Public License
 along with Christmas Countdown Widget. If not, see <http://www.gnu.org/licenses/>.
 */
- 
 
-class xmascount extends WP_Widget
-{
-  function xmascount()
+//Adds Countdown Shortcodes
+
+function cw_CountDownShortcode_left() {  
+    return '<div style="float:left;padding-right:15px;"><div id="cw_countdown"><div class="cw_countdown-text">
+          <script type="text/javascript">
+          <!--
+          cw_axmascount();
+          //--></script></div></div></div>';  
+}  
+add_shortcode('countdown', 'cw_CountDownShortcode_left');
+
+function cw_CountDownShortcode_right() {  
+    return '<div style="float:right;padding-left:15px;"><div id="cw_countdown"><div class="cw_countdown-text">
+          <script type="text/javascript">
+          <!--
+          cw_axmascount();
+          //--></script></div></div></div>';  
+}  
+add_shortcode('countdown-right', 'cw_CountDownShortcode_right');
+
+
+//Enqueue Countdown Scripts and Styles
+
+function cw_santas_countdown_scripts() {
+  wp_enqueue_style( 'xmascount-styles', plugins_url( 'css/cw_xmascount-styles.css', __FILE__ ) );
+  wp_enqueue_script( 'xmascount-scripts', plugins_url( 'scripts/scriptfile.js', __FILE__ ) );
+}
+add_action( 'wp_enqueue_scripts', 'cw_santas_countdown_scripts' );
+
+
+
+
+//Extends Countdown Widget
+
+class cw_axmascount extends WP_Widget {
+  function cw_axmascount()
   {
-    $widget_ops = array('classname' => 'xmascount', 'description' => 'Displays a cute Christmas countdown' );
-    $this->WP_Widget('xmascount', 'Christmas Countdown', $widget_ops);
+    $widget_ops = array('classname' => 'cw_axmascount', 'description' => 'Drag this widget to your sidebar to display Santa\'s Christmas Countdown.' );
+    $this->WP_Widget('cw_axmascount', 'Santa\'s Countdown', $widget_ops);
   }
  
   function form($instance)
@@ -36,9 +68,7 @@ class xmascount extends WP_Widget
     $instance = wp_parse_args( (array) $instance, array( 'title' => '' ) );
     $title = $instance['title'];
 ?>
-
-  <p><label for="<?php echo $this->get_field_id('title'); ?>">Title: <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo attribute_escape($title); ?>" /></label></p>
-
+  <p><label for="<?php echo $this->get_field_id('title'); ?>">Title: <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo esc_attr($title); ?>" /></label></p>
 <?php
 }
   function update($new_instance, $old_instance)
@@ -59,54 +89,14 @@ class xmascount extends WP_Widget
       echo $before_title . $title . $after_title;;
  
 
-    echo "<div style=\"background-image: url(".plugins_url('img/santas-countdown-background.png', __FILE__)."); 
-     margin: 0 auto;
-     height:191px; 
-     width:180px; 
-     background-repeat: no-repeat;
-     background-size: 180px 191px;
-     text-align:center;\"><div style=\"font-weight:normal;
-     padding-left:37px; 
-     font-size:15px; 
-     line-height:110%;
-     padding-right: 0px; 
-     padding-top:123px; 
-     text-align:center;
-     font-family: comic sans ms;
-     color: #0099CC;\"><script language=\"javascript\" type=\"text/javascript\">
-     today = new Date();
-     thismon = today.getMonth();
-     thisday = today.getDate();
-     thisyr = today.getFullYear();
-if (thismon == 11 && thisday > 25)
-	{
-	thisyr = ++thisyr;
-	BigDay = new Date(\"December 25, \"+thisyr);
-	}
-else
-	{
-	BigDay = new Date(\"December 25, \"+thisyr);
-	}
-
-        msPerDay = 24 * 60 * 60 * 1000;
-        timeLeft = (BigDay.getTime() - today.getTime());
-        e_daysLeft = timeLeft / msPerDay;
-        daysLeft = Math.floor(e_daysLeft);
-        e_hrsLeft = (e_daysLeft - daysLeft) * 24;
-        hrsLeft = Math.floor(e_hrsLeft);
-        minsLeft = Math.floor((e_hrsLeft - hrsLeft) * 60);
-if (daysLeft <= 0 )
-{ 
-document.write(\"Merry<br>Christmas!\")
-}
-else 
-{ 
-document.write(\"\" + daysLeft + \" days<BR>til Christmas!\"); 
-}
-    </script></div></div>";
+    echo '<div id="cw_countdown"><div class="cw_countdown-text">
+          <script type="text/javascript">
+          <!--
+          cw_axmascount();
+          //--></script></div></div>';
  
     echo $after_widget;
   }
  
 }
-add_action( 'widgets_init', create_function('', 'return register_widget("xmascount");') );?>
+add_action( 'widgets_init', create_function('', 'return register_widget("cw_axmascount");') );
